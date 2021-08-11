53953e21aee8e94095260fdc8e4b94c450e3992c
eom-yeji <eomyj0920@gamai.com>
2021-08-11 21:29:59 +0900

    FizzBuzz

---
+++ src/algorithm/FizzBuzz.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+public class FizzBuzz {
+    private static String toWord(int divisor,int value,String word){
+        return value%divisor==0 ? word:"";
+    }
+    public static void main(String[]args){
+        for(int i=1;i<=15;i++){
+            String word=toWord(3,i,"Fizz")+toWord(5,i,"Buzz");
+
+            if(!word.isEmpty()){
+                System.out.println(i+":"+word);
+            }
+        }
+    }
+}
