1ad9710f2cd37f94dbe8e43c2c0e1ddb4e18bbf5
yjeom <eomyj0920@gmail.com>
2021-08-16 14:41:54 +0900

    Builder Pattern 사용하지 않을 때

---
+++ src/Member.java
@@ -0,0 +1,22 @@
+public class Member {
+    private final String email;
+    private final String password;
+    private final String name;
+    private final String telephone;
+    private final String address;
+
+    public Member(String email,String password,String name,String telephone,String address){
+        this.email=email;
+        this.password=password;
+        this.name=name;
+        this.telephone=telephone;
+        this.address=address;
+
+    }
+}
+class MemberTest{
+    public static void main(String[]args){
+        final Member m=new Member("member1@test.com","1234"
+                ,"김아무개","010-1234-5678","서울");
+    }
+}
