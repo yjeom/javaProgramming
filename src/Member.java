3dd7d502afd01d8516fd2629edb696daa5feb04e
yjeom <eomyj0920@gmail.com>
2021-08-16 15:23:53 +0900

    Builder Pattern 사용할 때

--- src/Member.java
+++ src/Member.java
@@ -5,21 +5,38 @@ public class Member {
     private final String telephone;
     private final String address;
 
-    public Member(String email,String password,String name,String telephone,String address){
-        this.email=email;
-        this.password=password;
-        this.name=name;
-        this.telephone=telephone;
-        this.address=address;
+    public static class Builder{
+        private final String email;
+        private final String password;
+        private final String name;
+        private final String telephone;
+        private  String address;
 
+        public Builder(String email,String password,String name,String telephone){
+            this.email=email;
+            this.password=password;
+            this.name=name;
+            this.telephone=telephone;
+        }
+        public Builder address(String address){
+            this.address=address;
+            return this;
+        }
     }
-    public Member(String email,String password,String name,String telephone) {
-        this(email,password,name,telephone,"모름");
+    private Member(Builder builder){
+        email=builder.email;
+        password=builder.password;
+        name=builder.name;
+        telephone=builder.telephone;
+        address=builder.address;
     }
+
 }
 class MemberTest{
     public static void main(String[]args){
-        final Member m=new Member("member1@test.com","1234"
-                ,"김아무개","010-1234-5678","서울");
+        Member.Builder builder=new Member.Builder(
+                "member1@test.com","1234","김아무개","010-1234-5678");
+        builder.address("서울");
+
     }
 }
